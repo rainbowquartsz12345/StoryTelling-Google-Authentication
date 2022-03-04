42559969b34f5ad00112e6bf45b48fc0e6694490
rainbowquartsz12345 <78510158+rainbowquartsz12345@users.noreply.github.com>
2022-03-04 17:12:21 -0500

    Add files via upload

---
+++ screens/LoginScreen.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Button } from 'react-native';
+import firebase from "firebase"
+import * as Google from 'expo-google-app-auth';
+
+
+export default class LoginScreen extends Component {
+  isUserEqual = (googleUser, firebaseUser) => {
+
+    if (firebaseUser) {
+        const providerData = firebaseUser.providerData;
+        for (let i = 0; i < providerData.length; i++) {
+          if (providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+              providerData[i].uid === googleUser.getBasicProfile().getId()) {
+            // We don't need to reauth the Firebase connection.
+            return true;
+          }
+        }
+      }
+      return false;
+
+  };
+
+  onSignIn = (googleUser) => {
+    // We need to register an Observer on Firebase Auth to make sure auth is initialized.
+    var unsubscribe = firebase.auth().onAuthStateChanged((firebaseUser) => {
+      unsubscribe();
+      // Check if we are already signed-in Firebase with the correct user.
+      if (!this.isUserEqual(googleUser, firebaseUser)) {
+        var credential = firebase.auth.GoogleAuthProvider.credential(
+          googleUser.idToken,
+          googleUser.accessToken
+        );
+        firebase
+          .auth()
+          .signInWithCredential(credential)
+          .then(function (result) {
+            if (result.additionalUserInfo.isNewUser) {
+              firebase
+                .database()
+                .ref('/users/' + result.user.uid)
+                .set({
+                  gmail: result.user.email,
+                  profile_picture: result.additionalUserInfo.profile.picture,
+                  locale: result.additionalUserInfo.profile.locale,
+                  first_name: result.additionalUserInfo.profile.given_name,
+                  last_name: result.additionalUserInfo.profile.family_name,
+                  current_theme: 'dark',
+                })
+                .then(function (snapshot) {});
+            }
+          })
+          .catch((error) => {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+          });
+      } else {
+        console.log('User already signed-in Firebase.');
+      }
+    });
+  };
+
+  signInWithGoogleAsync = async () => {
+    try {
+      const result = await Google.logInAsync({
+        behavior: 'web',
+        androidClientId: '1056345973059-mai4cmge5k2q8dvogqet43609iu1ph1p.apps.googleusercontent.com',
+        iosClientId: '1056345973059-bs59s4n4pm3ocgplapsdqm8llrn4dnof.apps.googleusercontent.com',
+        scopes: ['profile', 'email'],
+      });
+      if (result.type === 'success') {
+        this.onSignIn(result);
+        this.props.navigation.navigate('DashboardScreen');
+        return result.accessToken;
+      } else {
+        return { cancelled: true };
+      }
+    } catch (e) {
+      console.log(e.message);
+      return { error: true };
+    }
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button title = "Sign In With Google" onPress = {()=> this.signInWithGoogleAsync()}/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
